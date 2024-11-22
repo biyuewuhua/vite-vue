6e37cb11a2571b091123adafb15b51ef706eb425
biyuewuhua <1669241937@qq.com>
2024-11-22 14:05:06 +0800

    导入组件

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import Header from './components/header.vue';
+    import Footer from './components/footer.vue';
+</script>
+
+<template>
+    <Header/>
+
+    dengruicode.com
+
+    <Footer/>
+</template>
+
+<style scoped>
+
+</style>
